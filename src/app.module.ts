44480d18861ef1eb6e9b532420ba249594404a0f
Syuja Makarim <syujamakarim13@gmail.com>
2026-02-12 10:53:42 +0700

    feat(auth): implement coresponding  modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -19,6 +21,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         synchronize: true,
       }),
     }),
+
+    PostsModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
