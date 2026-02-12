f0f03f8631721394a68801fdd9826f3e428238fa
Syuja Makarim <syujamakarim13@gmail.com>
2026-02-12 10:31:38 +0700

    feat(auth): configure jwt module and strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,42 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy/jwt.strategy';
+import { JwtModuleOptions } from '@nestjs/jwt';
+import type { StringValue } from 'ms';
 
 @Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService): JwtModuleOptions => {
+        const secret = config.get<string>('JWT_SECRET');
+        const expiresIn = config.get<string>('JWT_EXPIRES_IN');
+
+        if (!secret) {
+          throw new Error('JWT_SECRET is not defined');
+        }
+
+        if (!expiresIn) {
+          throw new Error('JWT_EXPIRES_IN is not defined');
+        }
+
+        return {
+          secret,
+          signOptions: {
+            expiresIn: expiresIn as StringValue,
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
